1592450666141a894fb3a2928bcd1d57105008d8
Kaycee Ilonzo <kay4murda@yahoo.com>
2015-06-16 09:36:18 +0000

    Wired collection into postsList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachareif.com/introducin-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	}
-];
 Template.postsList.helpers({
-	posts: postsData
+	posts: function () {
+		return Posts.find();
+	}
 });
