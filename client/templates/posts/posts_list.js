d9efef6857b6b36c067da87cf3358377f1feb429
Suleman Mohammed <sumohammed@hotmail.com>
2015-07-13 10:18:29 +0000

    Fade items in when they are dawn.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,6 +1,12 @@
 Template.postsList.onRendered(function () {
   this.find('.wrapper')._uihooks = {
-      moveElement: function (node, next) {
+  	insertElement: function (node, next) {
+  		$(node)
+  		  .hide()
+  		  .insertBefore(next)
+  		  .fadeIn();
+  	},
+    moveElement: function (node, next) {
       	var $node = $(node), $next = $(next);
       	var oldTop = $node.offset().top;
       	var height = $node.outerHeight(true);
@@ -33,6 +39,12 @@ Template.postsList.onRendered(function () {
 	    // reset everything to 0, animated
 	    $node.addClass('animate').css('top', 0);
 	    $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function (node) {
+    	$(node).fadeOut(function() {
+    		$(this).remove();
+    	});
     }
+
   }
 });
